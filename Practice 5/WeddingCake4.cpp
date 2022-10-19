4f88843ffe5bc9438e3b77ee8e3e173876c65776
mitkan191003 <mithrak8022@gmail.com>
2022-10-19 10:18:52 -0400

    Solved Wedding Cake
    Pointer solution in file 3 took over 1.5s on test 16, but using constant time ops in file 4 took 109ms.

--- Practice 5/WeddingCake4.cpp
+++ Practice 5/WeddingCake4.cpp
@@ -37,35 +37,21 @@ int main(){
 	ios_base::sync_with_stdio(false);
 	cin.tie(0);
 
-    int numLayers, layer;
-    cin >> numLayers;
-    list<int> started;
-    list<int>::iterator jdx = started.begin();
-    // int* jdi = &(*jdx);
-    int completed = 0;
-    for (int i = 0; i < numLayers; i++) {
+    vector<int> layers = {0, 0, 0, 0, 0};
+    int numDays, layer;
+    cin >> numDays;
+
+    for (int i = 0; i < numDays; i++) {
         cin >> layer;
         if (layer == 1) {
-            started.push_back(1);
-        } else if (started.size() != 0) {
-            jdx = started.begin();
-            for (int j = 0; j < started.size(); j++) {
-                if (layer == *jdx + 1) {
-                    *jdx = *jdx + 1;
-                    // jdi = &(*jdx);
-                    // cout << jdi - &(*started.begin()) << ": " << *jdx << endl;
-                    if (*jdx == 5) {
-                        completed++;
-                        started.erase(jdx);
-                    }
-                    break;
-                }
-                advance(jdx, 1);
-            }
+            layers[0]++;
+        } else if (layers[layer - 2] != 0) {
+            layers[layer - 2]--;
+            layers[layer - 1]++;
         }
     }
 
-    cout << completed << endl;
+    cout << layers[4] << endl;
 
 	return 0;
 }
